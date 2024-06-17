e7e90792633ed24da4f357257e4316f18d13a98b
DjoykeAbyah <crudendjoyke@gmail.com>
2024-06-17 15:08:21 +0200

    continue on laptop

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,9 +6,23 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/16 21:34:50 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/06/16 21:35:09 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/17 15:08:05 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+
+class Animal
+{
+	protected:
+		std::string		type;
+		
+	public:
+		Animal();
+		Animal(const std::string& string);//do i need to use this?
+		Animal(const Animal& copy);
+		const Animal& operator=(const Animal& copy);
+		~Animal();
+};
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,12 @@
 /*   By: djoyke <djoyke@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/16 21:34:56 by djoyke        #+#    #+#                 */
-/*   Updated: 2024/06/16 21:34:57 by djoyke        ########   odam.nl         */
+/*   Updated: 2024/06/17 13:23:41 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+int main()
+{
+	//polymorphism
+	//function overwriting
+}
